16267786980164763a2759e3553f21c657386f74
kemal <kemal.ediz@gmail.com>
2019-01-28 00:10:02 +0000

    some files to write into during the interview

---
+++ src/main/java/com/argos/interviews/luhnalgorithm/CreditCardValidatorService.java
@@ -0,0 +1,4 @@
+package com.argos.interviews.luhnalgorithm;
+
+public class CreditCardValidatorService {
+}
---
+++ src/test/java/com/argos/interviews/luhnalgorithm/CreditCardValidatorServiceTest.java
@@ -0,0 +1,7 @@
+package com.argos.interviews.luhnalgorithm;
+
+import static org.junit.Assert.*;
+
+public class CreditCardValidatorServiceTest {
+
+}
